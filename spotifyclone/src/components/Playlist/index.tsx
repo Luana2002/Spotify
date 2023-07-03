148aba91ddefe77bbf190c0085b901ed356e3d5d
luanaPaulaEdgeUfal <luana.paula@edge.ufal.br>
2023-07-03 09:48:05 -0300

    fix: corrige espaçamento

--- spotifyclone/src/components/Playlist/index.tsx
+++ spotifyclone/src/components/Playlist/index.tsx
@@ -9,7 +9,7 @@ interface Play {
 export function PlayList({ title, desc, image }: Play) {
     return (
         <>
-            <a href="" className="text-sm flex gap-4 text-zinc-300 hover:text-zinc-100">
+            <a href="" className="py-1 text-sm flex gap-4 text-zinc-300 hover:text-zinc-100">
                 <Image src={image} width={50} height={50} alt="Album mais curtidas" />
                 <div className="flex flex-col gap-2">
                     <strong className="font-semibold">{title}</strong>
